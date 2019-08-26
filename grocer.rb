4c1f5747afa1b94e9e5690ca9e07cf804784b103
Jeremy Amram <jeremy.j.amram@gmail.com>
2019-08-26 15:44:51 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,6 +1,18 @@
 def consolidate_cart(cart)
-  # code here
-end
+  cart = [
+        {"AVOCADO" => {:price => 3.00, :clearance => true}},
+        {"AVOCADO" => {:price => 3.00, :clearance => true}},
+        {"KALE" => {:price => 3.00, :clearance => false}},
+        ]
+
+  new_cart = {} 
+  cart.each do |items| 
+    items.each do |key, value| 
+      new_cart[key] ||= value 
+      new_cart[key][:count] ? new_cart[key][:count] += 1 :   
+      new_cart[key][:count] = 1 
+  end 
+end 
 
 def apply_coupons(cart, coupons)
   # code here
